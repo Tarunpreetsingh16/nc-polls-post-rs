2dd8e64c9b0503a884e5db3e82f480ef56bd331f
jimmy <tarunpreetsingh16@gmail.com>
2023-05-24 22:14:49 -0400

    added authorization rules and jwt filter to filter chain

---
+++ src/main/kotlin/com/neighborhood/connect/pollspostsrs/config/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package com.neighborhood.connect.pollspostsrs.config
+
+import com.neighborhood.connect.jwtlib.filters.JWTRequestFilter
+import com.neighborhood.connect.jwtlib.service.JWTService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.RegexRequestMatcher
+
+
+@Configuration
+class SecurityConfig(
+    private val jwtService: JWTService,
+    private val jwtRequestFilter: JWTRequestFilter
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeHttpRequests()
+            .requestMatchers("/polls/heartbeat").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+    @Throws(Exception::class)
+    fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(jwtService)
+            .passwordEncoder(passwordEncoder())
+    }
+
+    @Bean
+    @Throws(java.lang.Exception::class)
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager? {
+        return authenticationConfiguration.authenticationManager
+    }
+}
