c051ed173f296f3c6e47d124704fb9c9d00089fb
jimmy <tarunpreetsingh16@gmail.com>
2023-05-28 12:51:34 -0400

    add new mapping to create a post

--- src/main/kotlin/com/neighborhood/connect/pollspostsrs/controller/IPollsPostsRsController.kt
+++ src/main/kotlin/com/neighborhood/connect/pollspostsrs/controller/IPollsPostsRsController.kt
@@ -1,8 +1,13 @@
 package com.neighborhood.connect.pollspostsrs.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 interface IPollsPostsRsController {
     @GetMapping("/heartbeat", produces = ["application/json"])
     fun heartBeat(): String
+
+    @PostMapping("/post", produces = ["application/json"])
+    fun createPost(): ResponseEntity<Any>
 }
